33f06bcd979b0e42516a964d2ff7481e6b1f08f7
CodingPanda <email@example.com>
2025-06-17 09:34:28 +0800

    Learning os api

--- learnOS.py
+++ learnOS.py
@@ -7,3 +7,9 @@ print(os.getcwd())
 # 显示路径下所有文件和目录组成的列表
 path=r'D:\CodingPanda'
 print(os.listdir(path))
+
+
+for paths,dirs,files in os.walk(path):
+    print(path)
+    print(dirs)
+    print(files)
